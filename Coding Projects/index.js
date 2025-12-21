9048773f84436836528f21616bfe941498a2dc71
Samater Muhamed <samaden250@gmail.com>
2025-12-21 03:41:58 +0100

    Update index.js

--- Coding Projects/index.js
+++ Coding Projects/index.js
@@ -7,5 +7,5 @@ body.style.backgroundColor = name;
 function randomColor() {
 const red = Math.round(Math.random() * 255)
 const green = Math.round(Math.random() * 255)
-const blur = Math.round(Math.random() * 255)
+const blue = Math.round(Math.random() * 255)
 }
